1c868e53f1313620817df846e65bfb3eb58c3017
Glogzi <116293555+Glogzi@users.noreply.github.com>
2024-01-15 16:20:27 +0100

    Dwaring question added (it's not displayed tho)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,8 +15,35 @@ namespace flashcardsCsRewrite {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        public string[] QandA = File.ReadAllLines("QandA.txt");
         public MainWindow() {
             InitializeComponent();
+            //creating QandA file if not exist
+            doExist();
+        }
+        public static string randomQ(string[] questionsList) {
+            Random rnd = new Random();
+            string[] onlyQ = new string[questionsList.Length / 2];
+            //creating only question list
+            for (int question = 0; question < questionsList.Length; question += 2) {
+                onlyQ[question] = questionsList[question];
+            }
+            //drawing random question
+            int index = rnd.Next(0, onlyQ.Length);
+            string drawnQuestion = onlyQ[index];
+            return drawnQuestion;
+        }
+        public static void doExist() {
+            if (!File.Exists("QandA.txt")){
+                File.Create("QandA.txt");
+                System.Diagnostics.Process.Start("flashcardsCsRewrite");
+                Environment.Exit(0);
+            }            
+        }
+
+        private void submit_Click(object sender, RoutedEventArgs e) {
+            string showIt = Answer.Text.Trim();
+            Question.Text = showIt;
         }
     }
 }
