41676a5244204793ab1ab6c80cdddbea81bc2be5
Glogzi <116293555+Glogzi@users.noreply.github.com>
2024-01-15 16:51:29 +0100

    Questions is now displayed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,20 +20,31 @@ namespace flashcardsCsRewrite {
             InitializeComponent();
             //creating QandA file if not exist
             doExist();
+            string drawnedQ = randomQ(QandA);
+            Question.Text = drawnedQ;
         }
-        public static string randomQ(string[] questionsList) {
-            Random rnd = new Random();
-            string[] onlyQ = new string[questionsList.Length / 2];
-            //creating only question list
-            for (int question = 0; question < questionsList.Length; question += 2) {
-                onlyQ[question] = questionsList[question];
+        private static string randomQ(string[] questionsList) {
+            string drawnQuestion = null;
+            try {
+                while (drawnQuestion is null) {
+                    Random rnd = new Random();
+                    string[] onlyQ = new string[questionsList.Length];
+                    //creating only question list
+                    for (int question = 0; question < onlyQ.Length; question += 2) {
+                        onlyQ[question] = questionsList[question];
+                    }
+                    //drawing random question
+                    int index = rnd.Next(0, onlyQ.Length);
+                    drawnQuestion = onlyQ[index];
+                }
             }
-            //drawing random question
-            int index = rnd.Next(0, onlyQ.Length);
-            string drawnQuestion = onlyQ[index];
+            catch {
+                drawnQuestion = "please Fill QandA.txt file";
+            }
+
             return drawnQuestion;
         }
-        public static void doExist() {
+        private static void doExist() {
             if (!File.Exists("QandA.txt")){
                 File.Create("QandA.txt");
                 System.Diagnostics.Process.Start("flashcardsCsRewrite");
@@ -43,7 +54,8 @@ namespace flashcardsCsRewrite {
 
         private void submit_Click(object sender, RoutedEventArgs e) {
             string showIt = Answer.Text.Trim();
-            Question.Text = showIt;
+            string drawnedQ = randomQ(QandA);
+            Question.Text = drawnedQ;
         }
     }
 }
